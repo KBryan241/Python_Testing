640d5cc6c89f0d4e8f9971195671bd6b2a9bf96d
KBryan241 <koungabryan@gmail.com>
2024-02-06 13:55:55 +0000

    last

--- test/test_unitaires/test_purchase.py
+++ test/test_unitaires/test_purchase.py
@@ -43,3 +43,70 @@ class Test_purchased_place(unittest.TestCase):
         self.assertIn(b'Great-booking complete!',response.data)
 if __name__ == '__main__':
     unittest.main
+# class TestPurchasePlaces(unittest.TestCase):
+
+#     def setUp(self):
+#         app.config['TESTING'] = True
+#         self.client = app.test_client()
+#         self.competitions = comp
+#         self.clubs = c
+
+#     def test_purchasePlaces_success(self):
+#         # Arrange
+#         data = {
+#             'competition': 'Competition 1',
+#             'club': 'Club 1',
+#             'places': '5'
+#         }
+#         expected_message = 'Great-booking complete!'
+
+#         # Act
+#         response = self.client.post('/purchasePlaces', data=data, follow_redirects=True)
+#         actual_message = session.get('message', None)
+
+#         # Assert
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIn(expected_message, actual_message)
+#         self.assertEqual(self.competitions[0]['numberOfPlaces'], 5)
+#         self.assertEqual(self.clubs[0]['points'], 45)
+
+#     def test_purchasePlaces_not_enough_points(self):
+#         # Arrange
+#         data = {
+#             'competition': 'Competition 1',
+#             'club': 'Club 2',
+#             'places': '15'
+#         }
+#         expected_message = 'Your points is not enough'
+
+#         # Act
+#         response = self.client.post('/purchasePlaces', data=data, follow_redirects=True)
+#         actual_message = session.get('message', None)
+
+#         # Assert
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIn(expected_message, actual_message)
+#         self.assertEqual(self.competitions[0]['numberOfPlaces'], 10)
+#         self.assertEqual(self.clubs[1]['points'], 30)
+
+#     def test_purchasePlaces_no_places_available(self):
+#         # Arrange
+#         data = {
+#             'competition': 'Competition 1',
+#             'club': 'Club 1',
+#             'places': '15'
+#         }
+#         expected_message = 'No place available for this competition'
+
+#         # Act
+#         response = self.client.post('/purchasePlaces', data=data, follow_redirects=True)
+#         actual_message = session.get('message', None)
+
+#         # Assert
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIn(expected_message, actual_message)
+#         self.assertEqual(self.competitions[0]['numberOfPlaces'], 10)
+#         self.assertEqual(self.clubs[0]['points'], 50)
+
+# if __name__ == '__main__':
+#     unittest.main()
